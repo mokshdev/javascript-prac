90abff0555d264a59c89af44ae35df195651004a
mokshdev <mokshsingh12345@gmal.com>
2022-09-29 22:45:50 +0530

    practice

---
+++ practice.js
@@ -0,0 +1,36 @@
+// //   creating elements
+// const ul = document.querySelector('ul');
+// const li = document.createElement('li');
+
+// //   adding elements
+// ul.append(li)
+
+
+// //   modifying the text
+// // const firstListItem = document.querySelector('.list-items')
+
+// // console.log(firstListItem.innerText);
+// // console.log(firstListItem.textContent);
+// // console.log(firstListItem.innerHTML);
+
+// li.innerText = 'X-man';
+ 
+// //   Modifying attributes and classes
+
+// li.setAttribute('id','main-heading')
+// li.removeAttribute('id')
+
+// const title = document.querySelector('#main-heading')
+
+// console.log(title.getAttribute('id'));
+
+
+
+
+//   parent node traversal
+
+let ul  = document.querySelector('ul');
+
+
+console.log(ul.parentNode);
+console.log(ul.parentElement);
